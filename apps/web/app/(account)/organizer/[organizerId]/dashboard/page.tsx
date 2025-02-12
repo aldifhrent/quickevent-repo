7c935a071ff80328d4fb0c75c6e1eccd56c190d0
aldifhrent <aldifahriziilmawan@gmail.com>
2025-02-12 20:00:28 +0700

    remove: components or move other folder

--- apps/web/app/(account)/organizer/[organizerId]/dashboard/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function OrganizerIdPage() {
-  return <div></div>;
-}
--- apps/web/app/(event)/event/explore/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function ExplorePage() {
-  return (
-    <div>
-      <h1 className="w-fit p-1 text-3xl font-bold">Explore events</h1>
-    </div>
-  );
-}
